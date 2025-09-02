d4d1fe2250a6222399f7e0526e756c606e1838d8
Rishabh Singh <tcrr2005@gmail.com>
2025-09-02 14:55:11 +0530

    deployed both backend and frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'gp-talks.vercel.app',
+    origin: 'https://gp-talks.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
